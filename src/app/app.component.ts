d780d1266ce8ae8905f8869003db8949699c571e
ritwik <ritwik@github.com>
2018-03-13 09:21:12 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'tableapp';
+
+  //array of objects
+  private fieldArray: Array<any> = [];
+
+  //empty object
+  private newAttribute: any = {};
+
+  private rowArray: Array<any> = [];
+
+  private newRow: any = {};
+
+
+  addRowValue(){
+    this.rowArray.push(this.newRow);
+    this.newRow = {};
+  }
+
+  deleteRowValue(index){
+    this.rowArray.splice(index,1);
+  }
+
+  addFieldValue(){
+    this.fieldArray.push(this.newAttribute);
+    this.newAttribute = {};
+  }
+
+  deleteFieldValue(index){
+    this.fieldArray.splice(index,1);
+
+  }
 }
